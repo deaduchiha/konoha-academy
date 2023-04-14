46b601b424629a54f285ab2802e15f9125f0c892
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-04-15 02:42:51 +0330

    clean extra codes and install react query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useTranslation } from "next-i18next";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { t } = useTranslation();
+  console.log(t);
+
+  return (
+    <header>
+      <h1>{t("greeting")}</h1>
+    </header>
+  );
 };
 
 export default Home;
